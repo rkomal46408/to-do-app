838dccb3a014664668c2c0eca6c9a68c4ff184fc
rkomal46408 <166420567+rkomal46408@users.noreply.github.com>
2024-05-08 14:21:42 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 // Setting up the database connection
-$host = 'localhost';  // Your database host
-$dbname = 'u740934665_todo';  // Your database name
-$username = 'u740934665_todo';  // Your database username
-$password = '@Todo321';  // Your database password
+$host = 'sql213.infinityfree.com';  // Your database host
+$dbname = 'if0_36485857_todo';  // Your database name, ensure this matches exactly with what's listed in your hosting control panel
+$username = 'if0_36485857';  // Your database username
+$password = 'dxLiJrVmOOi';  // Your database password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
 } catch (PDOException $e) {
     die("Could not connect to the database $dbname :" . $e->getMessage());
 }
